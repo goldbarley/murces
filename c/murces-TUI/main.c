d2017df547df6f54a4cf605ad94913a7f5c87678
DeployedReject <mishravaibhav2048@gmail.com>
2026-05-17 12:09:36 +0530

    Fixed merge conflicts I think? would really appreciate a GUI here

--- c/murces-TUI/main.c
+++ c/murces-TUI/main.c
@@ -19,21 +19,18 @@ int main(void) {
   if (info.ctx.term_has_color) {
     start_color();
 
-<<<<<<< HEAD
-		init_color(MC_COLOR_DARK_BLUE, 0, 0, 666);
-		init_color(MC_COLOR_LIGHT_CYAN, 329, 992, 996);
-		init_color(MC_COLOR_LIGHT_GREY, 666, 666, 666);
-		init_color(MC_COLOR_BLACK_BROWN, 250, 120, 40);
-		init_color(MC_COLOR_VIOLET_BLUE, 372, 372, 956);
-		init_color(MC_COLOR_LITTLE_BROWN_RED, 666, 250, 80);
-=======
+    init_color(MC_COLOR_DARK_BLUE, 0, 0, 666);
+    init_color(MC_COLOR_LIGHT_CYAN, 329, 992, 996);
+    init_color(MC_COLOR_LIGHT_GREY, 666, 666, 666);
+    init_color(MC_COLOR_BLACK_BROWN, 250, 120, 40);
+    init_color(MC_COLOR_VIOLET_BLUE, 372, 372, 956);
+    init_color(MC_COLOR_LITTLE_BROWN_RED, 666, 250, 80);
     init_color(MC_COLOR_DARK_BLUE, 0, 0, 666);
     init_color(MC_COLOR_LIGHT_CYAN, 329, 992, 996);
     init_color(MC_COLOR_LIGHT_GREY, 666, 666, 666);
     init_color(MC_COLOR_BLACK_BROWN, 86, 86, 86);
     init_color(MC_COLOR_VIOLET_BLUE, 372, 372, 956);
     init_color(MC_COLOR_LITTLE_BROWN_RED, 666, 0, 0);
->>>>>>> 366e8b8 (Added global make file and compiler flags json)
 
     init_pair(CPID_STDSCR, MC_COLOR_LIGHT_CYAN, MC_COLOR_DARK_BLUE);
     init_pair(CPID_MM_DESC, MC_COLOR_BLACK_BROWN, MC_COLOR_LIGHT_GREY);
@@ -47,13 +44,10 @@ int main(void) {
     goto kill_em;
   }
 
-<<<<<<< HEAD
-	return ret;
-=======
+  return ret;
 kill_em:
   prc_destroy_context(&info.ctx);
   prc_kill_mother();
 
   return ret;
->>>>>>> 366e8b8 (Added global make file and compiler flags json)
 }
